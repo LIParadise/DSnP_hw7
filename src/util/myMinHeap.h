c6bd39437146355954a8d7b0d0e5f2e78ee65223
LIParadise <a58524andy@gmail.com>
2018-12-30 15:34:29 +0800

    heap-fix and some other heap utilities shall be okay.

--- src/util/myMinHeap.h
+++ src/util/myMinHeap.h
@@ -32,12 +32,15 @@ public:
    // TODO
    const Data& min() const;
    void insert(const Data& );
-   void delMin() { }
-   void delData(size_t i) { }
+   void delMin();
+   void delData(size_t );
 
 private:
    // DO NOT add or change data members
    vector<Data>   _data;
+
+   // helper functions.
+   void heapFix( size_t );
    
    // 0 begin root of the heap.
    // (2*n)+1 --> left child.
@@ -67,5 +70,58 @@ MinHeap<T>::insert( const T& other )
     s = parent;
   }
   _data[s] = other;
+}
+
+template <typename T>
+void
+MinHeap<T>::delMin()
+{
+  swap( _data[0], _data[_data.size()-1] );
+  _data.erase(_data.back());
+  heapFix();
+}
+
+template <typename T>
+void
+MinHeap<T>::delData( size_t s )
+{
+  swap( _data[s], _data[_data.size()-1] );
+  _data.pop_back();
+  heapFix( s );
+}
+
+template <typename T>
+void
+MinHeap<T>::heapFix( size_t idx)
+{
+  // idx marks the position which may not obbey heap rule.
+  //
+  // its data is the old _data.back(), thus must not be smaller
+  // than it's parent; i.e. it's parent is smaller than all its
+  // descendants.
+  // 
+  // just compare to idx to child of idx, find if idx is 
+  // actually invalid.
+  if( !( idx<_data.size()) )
+    return;
+
+  T bak = _data[idx];
+  while (idx < _data.size())
+  {
+    size_t tmp     = idx;
+    size_t childL  = 2*idx + 1;
+    size_t childR  = 2*idx + 2;
+    if( childL < _data.size() )
+      if( _data[childL] < _data[tmp] )
+        tmp = childL;
+    if( childR < _data.size() )
+      if( _data[childR] < _data[tmp] )
+        tmp = childR;
+    if( tmp == idx )
+      break;
+    _data[ idx ] = move( _data[ tmp ] );
+    idx = tmp;
+  }
+  _data[idx] = move( bak );
 }
 #endif // MY_MIN_HEAP_H
